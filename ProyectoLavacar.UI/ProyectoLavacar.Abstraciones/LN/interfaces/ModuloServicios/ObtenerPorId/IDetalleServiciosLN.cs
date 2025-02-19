2020d60a1def70865132d78f79c876ca6973a4fd
Nao <145625763+NaoCor@users.noreply.github.com>
2025-02-18 22:36:45 -0600

    visual de servicios  y  detalles

---
+++ ProyectoLavacar.UI/ProyectoLavacar.Abstraciones/LN/interfaces/ModuloServicios/ObtenerPorId/IDetalleServiciosLN.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoLavacar.Abstraciones.Modelos.ModeloServicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLavacar.Abstraciones.LN.interfaces.ModuloServicios.ObtenerPorId
+{
+    public  interface IDetalleServiciosLN
+    {
+        ServiciosDto Detalle(int idServicio);
+    }
+}
